2ac4cff93790a81894aaa1cf6f857306e45c78cb
Hyeonju Kang <46iz.us2@gmail.com>
2023-01-04 14:11:19 +0900

    Refactor: Refactoring by useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useMemo, useCallback } from 'react';
+import { useRef, useState, useMemo, useCallback, useReducer } from 'react';
 import './App.css';
 import Counter from './Counter';
 import CreateUser from './CreateUser';
@@ -14,25 +14,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: '',
-    email: '',
-    // id: ''
-  })
-
-  const { username, email } = inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs(inputs => ({
-      ...inputs,
-      [name]: value
-    }));
-  }, []);
-
-  const [users, setUsers] = useState([
+    email: ''
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -51,34 +38,147 @@ function App() {
       email: 'liz@example.com',
       active: false
     }
-  ]);
+  ]
+}
 
-  const nextId = useRef(4)
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT': //액션객체, input상태 업데이트
+      return {
+        ...state, //불변성을 지키기 위해 spread연산 사용
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user)
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const nextId = useRef(4);
+
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => { //재사용
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, []);
 
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    }
-    setUsers(users => users.concat(user))
-
-
-    setInputs({
-      username: '',
-      email: ''
-    })
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
+    });
     nextId.current += 1;
   }, [username, email]);
 
-  const onRemove = useCallback(id => {
-    setUsers(users => users.filter(user => user.id !== id))
-    //일치하지 않는 원소만 추출해 새 배열 만듦 (일치하면 제거)
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
   }, []);
 
-  const onToggle = useCallback(id => {
-    setUsers(users => users.map(user => user.id === id ? { ...user, active: !user.active } : user))
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
   }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  // const [inputs, setInputs] = useState({
+  //   username: '',
+  //   email: '',
+  //   // id: ''
+  // })
+
+  // const { username, email } = inputs;
+
+  // const onChange = useCallback(e => {
+  //   const { name, value } = e.target;
+  //   setInputs(inputs => ({
+  //     ...inputs,
+  //     [name]: value
+  //   }));
+  // }, []);
+
+  // const [users, setUsers] = useState([
+  //   {
+  //     id: 1,
+  //     username: 'velopert',
+  //     email: 'public.velopert@gmail.com',
+  //     active: true
+  //   },
+  //   {
+  //     id: 2,
+  //     username: 'tester',
+  //     email: 'tester@example.com',
+  //     active: false
+  //   },
+  //   {
+  //     id: 3,
+  //     username: 'liz',
+  //     email: 'liz@example.com',
+  //     active: false
+  //   }
+  // ]);
+
+  // const nextId = useRef(4)
+
+  // const onCreate = useCallback(() => {
+  //   const user = {
+  //     id: nextId.current,
+  //     username,
+  //     email
+  //   }
+  //   setUsers(users => users.concat(user))
+
+
+  //   setInputs({
+  //     username: '',
+  //     email: ''
+  //   })
+  //   nextId.current += 1;
+  // }, [username, email]);
+
+  // const onRemove = useCallback(id => {
+  //   setUsers(users => users.filter(user => user.id !== id))
+  //   //일치하지 않는 원소만 추출해 새 배열 만듦 (일치하면 제거)
+  // }, []);
+
+  // const onToggle = useCallback(id => {
+  //   setUsers(users => users.map(user => user.id === id ? { ...user, active: !user.active } : user))
+  // }, []);
 
   // const onEdit = useCallback(user => {
   //   setInputs({
@@ -98,7 +198,7 @@ function App() {
   //     id: ''
   //   })
   // }, [users, username, email, id]);
-  const count = useMemo(() => countActiveUsers(users), [users])
+  // const count = useMemo(() => countActiveUsers(users), [users])
 
   return (
     <>
@@ -106,10 +206,20 @@ function App() {
         username={username}
         email={email}
         onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div>활성 사용자 수 : {count} </div>
+
+      {/* <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
         onCreate={onCreate} />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
-      <div>활성 사용자 수 : {count} </div>
+      <div>활성 사용자 수 : {count} </div> */}
     </>
+
     // <Wrapper>
     //   <User />
     //   <InputSample />
--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './Counter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// ReactDOM.render(<Counter />, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <Counter />
+  <App />
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
